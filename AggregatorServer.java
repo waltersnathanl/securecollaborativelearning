b5e8f82eeaccbf84762e05db46759a8f6bbd2d3e
waltersnathanl <55595206+waltersnathanl@users.noreply.github.com>
2019-09-20 08:55:11 -0700

    Add files via upload

---
+++ AggregatorServer.java
@@ -0,0 +1,107 @@
+import java.net.*;
+import java.io.*;
+
+import paillierp.Paillier;
+import paillierp.PaillierThreshold;
+import paillierp.key.KeyGen;
+import paillierp.key.PaillierPrivateThresholdKey;
+import paillierp.zkp.DecryptionZKP;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Random;
+
+import java.text.*;
+import java.util.*;
+
+public class AggregatorServer{
+    public static void main(String[] args){
+        //Task 1: get a list we can use to communicate with all the clients.
+        InetAddress[] clients = new InetAddress [3]; //TODO: put in the IP addresses
+
+
+        //Task 1: generate and distribute keys
+        Random rnd = new Random();
+        PaillierPrivateThresholdKey[] keys = KeyGen.PaillierThresholdKey(128,clients.length,clients.length,rnd.nextLong());
+        //currently we have it set so everyone needs to work together to decrypt anything.
+        PaillierThreshold myKey = new PaillierThreshold(keys[0]);
+        //I'm totally stealing one of the private keys so I can do my own crypto.  Shhh!
+        PaillierPrivateThresholdKey[] privateKeys = new PaillierPrivateThresholdKey[clients.length];
+        for(int i=0,i<keys.length,i++){
+            privateKeys[i] = keys[i].getPrivateKey();
+        }
+        String currentResponse;
+        for(int i=0,i<clients.length;i++){
+            try (Socket socket = new Socket(clients[i],8080)){
+                OutputStream outputStream = socket.getOutputStream();
+                InputStream inputStream = socket.getInputStream();
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                objectOutputStream.writeObject(privateKeys[i]);
+                currentResponse = (String) objectInputStream.readObject();
+            }
+        }
+
+
+        //Task 2: distribute the query and acquire coded responses
+        //subtask 1: create the query
+        String query = "qSELECT cancer_events, cancer_total, normal_events, normal_total FROM healthdata;&&&cancer_events:1&&&cancer_total:1&&&normal_events:1&&&normal_total:1";
+        int length_of_response = 4;  //I'm not sure what we'll do if not hardcode every query.
+
+        //subtask 2: collect encrypted responses
+        BigInteger [][] responseMatrix = new BigInteger[clients.length][];
+        for(int i=0,i<clients.length;i++){
+            try (Socket socket = new Socket(clients[i],8080)){
+                OutputStream outputStream = socket.getOutputStream();
+                InputStream inputStream = socket.getInputStream();
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                objectOutputStream.writeObject(query);
+                currentResponse = (BigInteger[length_of_response]) objectInputStream.readObject();
+                responseMatrix[i] = currentResponse;
+            }
+        }
+        //Task 3: aggregate the responses
+        BigInteger[] aggregatedResponses = new BigInteger[length_of_response];
+        for(int j=0;j<length_of_response;j++){
+            BigInteger current_value = responseMatrix[0][j];
+            for(i=1;i<clients.length;i++){
+                current_value = myKey.add(current_value,responseMatrix[i][j]);
+            }
+            aggregatedResponses[j] = current_value;
+        }
+
+        //Task 4: distribute the aggregates and decipher
+        BigInteger[] cleartextAggregates = new BigInteger[length_of_response];
+        for(int j=0;j<length_of_response;j++){
+            DecryptionZKP[] partialDecryptions = new DecryptionZKP[number_of_clients];
+            for(int i=0,i<clients.length;i++){
+                try (Socket socket = new Socket(clients[i], 8080)) {
+                    OutputStream outputStream = socket.getOutputStream();
+                    InputStream inputStream = socket.getInputStream();
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                    ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                    objectOutputStream.writeObject("e" + aggregatedResponses.toString());
+                    currentResponse = (DecryptionZKP) objectInputStream.readObject();
+                    partialDecryptions[i] = currentResponse;
+                }
+            }
+            cleartextAggregates[j] = myKey.combineShares(partialDecryptions);
+        }
+        //Task 5: Final processing.
+        //order is cancer_events, cancer_total, normal_events, normal_total
+        double oddsRatio = double(cleartextAggregates[0]*cleartextAggregates[3]/(double(cleartextAggregates[1]) * double(cleartextAggregates[2]));
+        System.out.println("The odds ratio is " + oddsRatio);
+        for(int i=0,i<clients.length;i++){
+            try (Socket socket = new Socket(clients[i],8080)){
+                OutputStream outputStream = socket.getOutputStream();
+                InputStream inputStream = socket.getInputStream();
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                objectOutputStream.writeObject("k");
+                currentResponse = (BigInteger[length_of_response]) objectInputStream.readObject();
+            }
+        }
+    }
+}
+
---
+++ ClientServer.java
@@ -0,0 +1,117 @@
+import java.net.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.sql.*;
+
+
+import java.math.BigInteger;
+import java.lang.Math;
+
+import paillierp.Paillier;
+import paillierp.PaillierThreshold;
+import paillierp.key.KeyGen;
+import paillierp.key.PaillierPrivateThresholdKey;
+import paillierp.zkp.DecryptionZKP;
+
+
+public class ClientServer {
+
+    public static double laplace(double scale) {
+        double exponential_sample1 = -scale * Math.log(Math.random());
+        double exponential_sample2 = -scale * Math.log(Math.random());
+        return exponential_sample1 - exponential_sample2;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //if (args.length < 1) return;
+        //int port = Integer.parseInt(args[0]);
+        int port = 8080;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+//            while(true){
+            Socket socket = serverSocket.accept();
+
+            OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            PaillierPrivateThresholdKey privateKey = (PaillierPrivateThresholdKey) objectInputStream.readObject();
+            Paillier encryptionKey = new Paillier(privateKey.getPublicKey());
+            PaillierThreshold decryptionKey = new PaillierThreshold(privateKey);
+
+            objectOutputStream.writeObject("confirmed");
+
+            socket.close();
+            while (true) {
+                Socket socket1 = serverSocket.accept();
+
+                OutputStream outputStream1 = socket.getOutputStream();
+                InputStream inputStream1 = socket.getInputStream();
+                ObjectOutputStream objectOutputStream1 = new ObjectOutputStream(outputStream);
+                ObjectInputStream objectInputStream1 = new ObjectInputStream(inputStream);
+
+                String serverMessageAndType = (String) objectInputStream.readObject();
+
+                char messageType = serverMessageAndType.charAt(0);
+                String serverMessage = serverMessageAndType.substring(1);
+                switch (messageType) {
+                    case 'q': //query
+
+                            //create the database connection -- we'll use same basic configuration for each database here
+                            //fancy future version will have something like a JSON table that has db info, aliases, etc
+                            // create our mysql database connection
+                            String[] queryAndNoise = serverMessage.split("&&&");
+                            //We will have query &&& result1:epsilon1 &&& result2:epsilon2...
+                            //Note that result is always an integer, because that's all we can encode with this system
+                            String query = queryAndNoise[0];
+                            String myDriver = "com.mysql.jdbc.Driver";
+                            String myUrl = "jdbc:mysql://localhost/testdb";
+                            Class.forName(myDriver);
+                            Connection conn = DriverManager.getConnection(myUrl, "testuser", "password");
+
+                            //perform the query.  Ideally the query can be stated precisely in terms of SQL statements.
+                            //If we need to do further processing, this would be where we implement that
+                            Statement statement = conn.createStatement();
+                            ResultSet results = statement.executeQuery(query);
+
+                            //Add noise according to sensitivity; it is the Aggregator's job to allocate this
+                            BigInteger[] encrypted_response = new BigInteger[queryAndNoise.length - 1];
+                            for (int i = 1; i < queryAndNoise.length; i++) {
+                                String[] dict = queryAndNoise[i].split(":");
+                                double noise = laplace(Double.parseDouble(dict[1]));
+                                int aggregate = results.getInt(dict[0]);
+                                BigInteger aggregatePlusNoise = BigInteger.valueOf(Math.round(aggregate + noise) * 100);
+                                BigInteger encrypted_message = encryptionKey.encrypt(aggregatePlusNoise);
+                                encrypted_response[i - 1] = encrypted_message;
+
+                                //]Remember we're sending back 100 times the values we're looking for.
+                                // For purposes of odds ratios this won't be problematic but it can show up elsewhere.
+                            }
+                            objectOutputStream.writeObject(encrypted_response);
+
+
+
+
+                    case 'e': //encrypted text
+                        //perform a partial decryption and return
+                        objectOutputStream.writeObject(decryptionKey.decryptProof(new BigInteger(serverMessage)));
+                    case 'k':
+                        socket.close();
+                        return;
+                }
+                objectOutputStream.close();
+                objectInputStream.close();
+                socket.close();
+                serverSocket.close();
+            }
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+
